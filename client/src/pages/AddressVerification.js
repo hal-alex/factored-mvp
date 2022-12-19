6a0f1b07e6bb06ff2e2e54a3143c9d3c06682553
Alex <1995aha@gmail.com>
2022-12-19 23:48:17 +0000

    address history - added barebones logic for dynamic form inputs

--- client/src/pages/AddressVerification.js
+++ client/src/pages/AddressVerification.js
@@ -1,23 +1,21 @@
 import React from 'react'
 import { useState, useEffect } from 'react'
+import { useGlobalContext } from '../context'
 
 const AddressVerification = () => {
 
+  const { userProfileTestData, setUserProfileTestData } = useGlobalContext()
   const [duration, setDuration] = useState(0)
-  const [addressCount, setAddressCount] = useState(1)
-
-  const addressFields = {
-    firstLine: "",
-    secondLine: "",
-    postcode: "",
-    townOrCity: "",
-    country: "",
-    startDate: "",
-    endDate: ""
-  }
-
   const [addressHistory, setAddressHistory] = useState([
-    addressFields
+    {
+      firstLine: "",
+      secondLine: "",
+      postcode: "",
+      townOrCity: "",
+      country: "",
+      startDate: "",
+      endDate: "",
+    },
   ])
 
   useEffect(() => {
@@ -30,13 +28,41 @@ const AddressVerification = () => {
         12 * (endAddressYear - startAddressYear)
       setDuration(differenceMonths)
     }
+    if (duration >= 36) {
+      console.log("address hit")
+      setUserProfileTestData({ ...userProfileTestData, "hasAddressHistory": true })
+    }
   }, [addressHistory])
 
-
-  const handleChange = (e) => {
-    setAddressHistory({ ...addressHistory, [e.target.name]: e.target.value })
+  const addMoreAddressFields = () => {
+    let newAddress = {
+      firstLine: "",
+      secondLine: "",
+      postcode: "",
+      townOrCity: "",
+      country: "",
+      startDate: "",
+      endDate: "",
+    }
+    setAddressHistory([...addressHistory, newAddress])
   }
 
+  const handleChange = (index, e) => {
+    let data = [...addressHistory]
+    data[index][e.target.name] = e.target.value
+    setAddressHistory(data)
+  }
+
+  const submitForm = (e) => {
+    e.preventDefault()
+
+  }
+
+  const removeAddress = (index) => {
+    let data = [...addressHistory]
+    data.splice(index, 1)
+    setAddressHistory(data)
+  }
 
   return (
     <div className="dashboard-div">
@@ -45,9 +71,27 @@ const AddressVerification = () => {
       <p>So far, we have address history for you for {duration} months</p>
       {duration >= 36 && <p>Address history is good</p>
         || <p>Please add more addresses</p>}
-      <button onClick={() => setAddressCount(addressCount + 1)}>Add another address</button>
+      <button onClick={addMoreAddressFields}>Add another address</button>
       <div>
-        <form className="form-container">
+        <form className="form-container" onSubmit={submitForm}>
+          {addressHistory.map((address, index) => {
+            const { firstLine, startDate, endDate } = address
+            return (
+              <div key={index}>
+                <input
+                  type="text"
+                  name="firstLine"
+                  value={firstLine}
+                  placeholder="First Address Line"
+                  onChange={(e) => handleChange(index, e)}
+                />
+                <button onClick={removeAddress}>Remove Address</button>
+              </div>
+            )
+          })}
+          <button onClick={submitForm}>Submit Address History</button>
+        </form>
+        {/* <form className="form-container">
           <label> First Line Address
             <input
               name='firstLine'
@@ -106,7 +150,7 @@ const AddressVerification = () => {
           </label>
           {duration ? <p>You have spent {duration} months at this address</p> : ""}
           <button>Confirm address</button>
-        </form>
+        </form> */}
       </div>
     </div>
   )
