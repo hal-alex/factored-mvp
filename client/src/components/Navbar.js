f18c9dd9b10703d13c99cca9ea325d8b01c657e7
Alex <1995aha@gmail.com>
2022-12-27 01:06:58 +0000

    some styling for the navbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,19 +1,27 @@
 import React from 'react'
 import { NavLink } from "react-router-dom"
+import LOGO from "../assets/factored_logo.png"
 
 const Navbar = () => {
   return (
-    <nav className="">
-        <div className="">
-            <ul className="nav-bar-links">
-                <NavLink to="/">Homepage</NavLink>
-                <NavLink to="/register">Register</NavLink>
-                <NavLink to="/login">Login</NavLink>
-                <NavLink to="/dashboard">Dashboard</NavLink>
-                <NavLink to="/profile">Profile</NavLink>
-            </ul>
-        </div>
-    </nav>
+    <header className="main-header">
+      <div className="header-logo">
+        <NavLink to="/" className="main-header__brand">
+          <img src={LOGO} alt="" />
+        </NavLink>
+      </div>
+      <nav className="main-navbar">
+        <ul class="main-navbar__items">
+          {/* <ul className="nav-bar-links"> */}
+          <li className="main-navbar__item"><NavLink to="/">Homepage</NavLink></li>
+          <li className="main-navbar__item"><NavLink to="/register">Register</NavLink></li>
+          <li className="main-navbar__item"><NavLink to="/login">Login</NavLink></li>
+          <li className="main-navbar__item"><NavLink to="/dashboard">Dashboard</NavLink></li>
+          <li className="main-navbar__item"><NavLink to="/profile">Profile</NavLink></li>
+        </ul>
+      </nav>
+    </header>
+
   )
 }
 
