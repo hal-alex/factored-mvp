97a362d4aa09f28b15e93a0e0f2e3d92b6844a94
Alex <1995aha@gmail.com>
2023-01-06 23:06:04 +0000

    added handling login and logout events

--- client/src/auth/auth.js
+++ client/src/auth/auth.js
@@ -1,3 +1,6 @@
+import Buffer from ""
+import { useNavigate } from "react-router-dom"
+
 export const getLocalToken = () => {
     window.localStorage.getItem("token")
 }
@@ -7,10 +10,24 @@ export const validateOrFetchToken = () => {
     if (!localToken) {
         return
     }
+    const splitLocalToken = localToken.split(".")
     if (splitLocalToken.length !== 3) {
         return
     }
-    const splitLocalToken = localToken.split(".")
-
+    return JSON.parse(Buffer.from(splitLocalToken[1], "base64"))
 }
 
+export const isAuthenticated = () => {
+    const userPayload = validateOrFetchToken()
+    if (!userPayload) return
+    const currentTime = Math.round(Date.now() / 1000)
+    return currentTime < userPayload.exp
+}
+
+export const handleLogout = () => {
+
+    const navigate = useNavigate()
+
+    window.localStorage.removeItem("token")
+    navigate("/")
+}
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,8 +1,15 @@
 import React from 'react'
 import { NavLink } from "react-router-dom"
 import LOGO from "../assets/factored_logo.png"
+import { isAuthenticated, handleLogout } from "../auth/auth"
+import { useEffect, useLocation } from 'react'
 
 const Navbar = () => {
+
+  const location = useLocation()
+
+  useEffect(() => { }, [location.pathname])
+
   return (
     <header className="main-header">
       <div className="header-logo">
@@ -14,10 +21,17 @@ const Navbar = () => {
         <ul className="main-navbar__items">
           {/* <ul className="nav-bar-links"> */}
           <li className="main-navbar__item"><NavLink to="/">Homepage</NavLink></li>
-          <li className="main-navbar__item"><NavLink to="/register">Register</NavLink></li>
-          <li className="main-navbar__item"><NavLink to="/login">Login</NavLink></li>
+
           <li className="main-navbar__item"><NavLink to="/dashboard">Dashboard</NavLink></li>
           <li className="main-navbar__item"><NavLink to="/profile">Profile</NavLink></li>
+          {isAuthenticated() ?
+            <p onClick={handleLogout}>Log out</p>
+            :
+            <>
+              <li className="main-navbar__item"><NavLink to="/register">Register</NavLink></li>
+              <li className="main-navbar__item"><NavLink to="/login">Login</NavLink></li>
+            </>
+          }
         </ul>
       </nav>
     </header>
